ef5e9fb31fefd1825f46bee822caa2d85525af55
Raphael Clement <raphael.clement0+1@gmail.com>
2026-05-12 16:01:31 +0200

    better file

--- generator.py
+++ generator.py
@@ -1,21 +1,17 @@
 from jinja2 import Template
-
-with open('pod.conf.j2', 'r') as fichier_template:
-   value = fichier_template.read()
+with open('pod.conf.j2', 'r') as file_template:
+   value = file_template.read()
 gen_jinja = Template(value)
-
 data = {
-    "nom_du_pod": "test-auto",
+    "name_pod": "test-auto",
     "type_app": "frontend",
-    "nom_du_conteneur": "nginx-serv",
-    "nom_image": "nginx",
+    "name_container": "nginx-serv",
+    "name_image": "nginx",
     "version_image": "alpine",
-    "port_interne": 80
+    "port": 80
 }
-
 yaml_final = gen_jinja.render(data)
-nom_fichier_sortie = "deploiement_genere.yaml"
-with open(nom_fichier_sortie, 'w') as fichier_sortie:
+end_file = "deploy_pod.yaml"
+with open(end_file, 'w') as fichier_sortie:
     fichier_sortie.write(yaml_final)
-
 print(f"done")
